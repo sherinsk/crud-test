13ed14c050cd776e6777a7082535c979978b4f68
Correct Username <correct@example.com>
2024-07-29 23:28:25 +0530

    jnfdjnx

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require('dotenv').config({ path: '../.env' });
 const express = require("express");
 const cloudinary = require("cloudinary").v2;
 const cors = require("cors");
